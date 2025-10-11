412449ec59ac287929721d8024d3efaee8b55cc4
@eyobs <eyobs573@gmail.com>
2025-10-11 15:04:27 +0300

    lern: learned about dynamic routing usind [id] and about params

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,7 +1,10 @@
-
-
-
+// ...existing code...
+import Link from 'next/link';
 export default function Page({ params }: { params: { id: string } }) {
-
-  return <div>Dynamic Route: {params.id}</div>
-}
+ return(
+  <>
+  <Link href={`/${params.id}/string`}>new</Link>
+  <div>Dynamic Page ID: {params.id}</div>
+  <Link href={`/${params.id}/number`}>Go to Number 123</Link>
+  </>
+ )}
